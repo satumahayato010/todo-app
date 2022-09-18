a95d789c8bbebc9fa8f6fe0ea54d0eb0599b7280
stm33 <satuma33@icloud.com>
2022-09-18 23:37:14 +0900

    [add]refs #3 add-event-listener

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const todoUL = document.getElementById("todos")
 
 const todos = JSON.parse(localStorage.getItem("todos"))
 
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
 function updateLS() {
 
     // liタグの要素を全部取得してくる。
@@ -23,3 +27,51 @@ function updateLS() {
     // ローカルストレージにtodosっていうキーでデータを保存する。
     localStorage.setItem("todos", JSON.stringify(todos))
 }
+
+form.addEventListener("submit", (e) => {
+    // デフォルトの動きをキャンセルする。
+    // ここでは、submitした時にページがリダイレクトされるのでキャンセルしてる。
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo) {
+    // 入力文字を取得
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+    if(todoText) {
+        // liタグを作成する。
+        const todoEl = document.createElement("li")
+        // タスク完了かをチェック
+        // todoはオブジェクトを格納でき、completedを持っている。
+        if(todo && todo.completed) {
+            todoEl.classList.add("completed")
+        }
+        todoEl.innerText = todoText
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed")
+
+            updateLS()
+        })
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+
+        })
+        todoUL.appendChild(todoEl)
+
+        input.value = ''
+
+        updateLS()
+    }
+}
+
+
+
